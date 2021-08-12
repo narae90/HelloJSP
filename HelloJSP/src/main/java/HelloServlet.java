bbe513fbbe7cef123be154a0044f24b78c312f4d
김나래 <narae900@naver.com>
2021-08-12 13:01:14 +0900

    첫 번째 서블릿

--- HelloJSP/src/main/java/HelloServlet.java
+++ HelloJSP/src/main/java/HelloServlet.java
@@ -6,26 +6,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//	서블릿은 HttpServlet을 상속
+public class HelloServlet extends HttpServlet {
 
-// 서블릿은 HrrpServlet을 상속
-public class HelloServlet extends HttpServlet{
-
-	
-	// GET 방식의 요청을 처리
+	//	GET 방식의 요청을 처리
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// 파라미터 확인
+	protected void doGet(HttpServletRequest req, 
+			HttpServletResponse resp) throws ServletException, IOException {
+		//	파라미터 확인
 		String name = req.getParameter("name");
 		if (name == null) {
 			name = "Anonymous";
 		}
-		
-		// 응답 출력을 위한 Writer
+		//	응답 출력을 위한 Writer
 		PrintWriter out = resp.getWriter();
-		out.print("<h3>Hello Servlet</h3>");
-		out.print("<p>Welcome," + name + "</p>");
-		super.doGet(req, resp);
+		out.println("<h3>Hello Servlet</h3>");
+		out.println("<p>Welcome, " + name + "</p>");
 	}
-	
 
 }
