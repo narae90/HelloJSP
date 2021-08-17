f978ebdbfa2e093c823d2f625fe1bc37e58f9423
김나래 <narae900@naver.com>
2021-08-17 14:47:08 +0900

    UserDao : 사용자 찾기

---
+++ HelloJSP/src/main/java/com/example/emaillist/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.example.emaillist.dao;
+
+import com.example.emaillist.vo.UserVo;
+
+public interface UserDao {
+	public UserVo getUserByEmailAndPassword(String email, String password);
+	public int insert(UserVo vo);
+	
+
+}
